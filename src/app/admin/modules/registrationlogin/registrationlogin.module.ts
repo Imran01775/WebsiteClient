da9c7f24af4cf6654ffc6c75e3cb0505ef78a7cd
imran01775 <inimranali48@gmail.com>
2020-11-05 00:27:48 +0600

    route module

---
+++ src/app/admin/modules/registrationlogin/registrationlogin.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from '../login/login.component';
+import { RegistrationComponent } from '../registration/registration.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: '', component: LoginComponent, pathMatch: 'full' },
+      { path: 'login', component: LoginComponent, pathMatch: 'full' },
+      { path: 'registration', component: RegistrationComponent }
+    ]
+  }
+];
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegistrationComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class RegistrationloginModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './admin/modules/registration/registration.component';
+import { LoginComponent } from './admin/modules/login/login.component';
+import { RegistrationloginModule } from './admin/modules/registrationlogin/registrationlogin.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RegistrationloginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
