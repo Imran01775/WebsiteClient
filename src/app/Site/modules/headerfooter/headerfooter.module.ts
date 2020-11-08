f87bb9a66695a83b6d7639f1f1562ff72bcef8b9
imran01775 <inimranali48@gmail.com>
2020-11-08 15:01:09 +0600

    push changes url

--- src/app/Site/modules/headerfooter/headerfooter.module.ts
+++ src/app/Site/modules/headerfooter/headerfooter.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { HeaderComponent } from '../header/header.component';
+import { HeaderComponent } from '../header/header.component'
 import { FooterComponent } from '../footer/footer.component';
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RegistrationComponent } from './admin/modules/registration/registration.component';
-import { LoginComponent } from './admin/modules/login/login.component';
 import { RegistrationloginModule } from './admin/modules/registrationlogin/registrationlogin.module';
-import { HeaderComponent } from './site/modules/header/header.component';
-import { FooterComponent } from './site/modules/footer/footer.component';
 import { HeaderfooterModule } from './Site/modules/headerfooter/headerfooter.module';
 
 @NgModule({
