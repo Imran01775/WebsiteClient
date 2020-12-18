b4d998f1b62e1b78afd59b8ff67480b5c09a2c4e
Sumaiya jouty <farhanajouty87@gmail.com>
2020-12-19 01:57:16 +0600

    add checkout and category

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,10 @@ import { HomeModule } from './Site/modules/home/home.module';
 import { RegistrationloginModule } from './admin/modules/registrationlogin/registrationlogin.module';
 import { HeaderfooterModule } from './Site/modules/headerfooter/headerfooter.module';
 import { ProductModule } from './Site/modules/product/product.module';
-<<<<<<< HEAD
 import { CategoryModule } from './Site/modules/category/category.module';
 import { CheckoutModule } from './Site/modules/checkout/checkout.module';
-=======
 import { ProductAdminModule } from './admin/modules/product/product-admin.module';
 
->>>>>>> a45f24c8674d95f0b9f56cfc1aed374d16d4d65b
 
 @NgModule({
   declarations: [
@@ -26,12 +23,9 @@ import { ProductAdminModule } from './admin/modules/product/product-admin.module
     RegistrationloginModule,
     HeaderfooterModule,
     ProductModule,
-<<<<<<< HEAD
     CategoryModule,
-    CheckoutModule
-=======
+    CheckoutModule,
     ProductAdminModule
->>>>>>> a45f24c8674d95f0b9f56cfc1aed374d16d4d65b
   ],
   providers: [],
   bootstrap: [AppComponent]
